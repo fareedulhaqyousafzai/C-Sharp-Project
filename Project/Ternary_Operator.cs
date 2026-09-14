3b28cb4661fc0e1da56de15d10ec097808e20177
fareedulhaq <fareedulhaqyousafzai@gmail.com>
2026-09-14 21:07:08 +0500

    Add project files.

---
+++ Project/Ternary_Operator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project
+{
+    internal class Ternary_Operator
+    {
+   
+        internal static void CheckAge()
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            string result = (age >= 18) ? "You are Eligible for voting." : "You are not eligible for voting.";
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Project/Unary_Operators.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project
+{
+    internal class Unary_Operators
+    {
+     internal static void PostIncrement()
+        {
+            int a = 10;
+            int b = a++ + a++ + a++;
+
+            Console.WriteLine("PostIncrement");
+            Console.WriteLine($"Value of a: {a}"); 
+            Console.WriteLine($"Value of b: {b}"); 
+        }
+        internal static void PreIncrement()
+        {
+            int a = 10;
+            int b = ++a + ++a + ++a;
+            Console.WriteLine("PreIncrement");
+            Console.WriteLine($"Value of a: {a}");
+            Console.WriteLine($"Value of b: {b}");
+        }
+        internal static void PostDecrement()
+        {
+            int a = 10;
+            int b = a-- + a-- + a--;
+
+            Console.WriteLine("PostDecrement");
+            Console.WriteLine($"Value of a: {a}");
+            Console.WriteLine($"Value of b: {b}");
+        }
+        internal static void PreDecrement()
+        {
+            int a = 10;
+            int b = --a + --a + --a;
+            Console.WriteLine("PreDecrement");
+            Console.WriteLine($"Value of a: {a}");
+            Console.WriteLine($"Value of b: {b}");
+        }
+    }
+}
